e9762abd8af105e1c8e0249f8420a90287a10d69
Chris Cassano <glitch0@gmail.com>
2017-12-14 19:33:55 -0800

    moved back and fwb buttons to top

--- AddressBar.js
+++ AddressBar.js
@@ -5,12 +5,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {
         TextInput,
-        View
+        View,
+        Image
         } from 'react-native'
 
 import BaseComponent from './BaseComponent'
 import Utils from './Utils'
 import styles from './styles'
+import Button from './Button'
 
 const TEXT_INPUT_REF = 'urlInput'
 
@@ -20,8 +22,10 @@ class AddressBar extends BaseComponent {
 
     this.inputText = ''
 
+    let toolbarState = AddressBar.updateState(this.props)
     this.state = {
-      url: this.props.url
+      url: this.props.url,
+      ...toolbarState
     }
 
     this._bind(
@@ -30,9 +34,22 @@ class AddressBar extends BaseComponent {
         )
   }
 
+  static updateState (props) {
+    return {
+      backButtonEnabled: props.backButtonEnabled,
+      forwardButtonEnabled: props.forwardButtonEnabled,
+      homeButtonEnabled: props.homeButtonEnabled,
+      onBack: props.onBack,
+      onHome: props.onHome,
+      onForward: props.onForward
+    }
+  }
+
   componentWillReceiveProps (nextProps) {
+    let toolbarState = AddressBar.updateState(this.props)
     this.setState({
-      url: nextProps.url
+      url: nextProps.url,
+      ...toolbarState
     })
   }
 
@@ -56,6 +73,10 @@ class AddressBar extends BaseComponent {
     this.refs[TEXT_INPUT_REF].blur()
   }
 
+  buttonStyle () {
+    return [styles.toolBarIcons, this.props.foregroundColor && {tintColor: this.props.foregroundColor}]
+  }
+
   render () {
     return (
       <View style={[styles.addressBarRow]}>
@@ -70,6 +91,25 @@ class AddressBar extends BaseComponent {
           underlineColorAndroid='white'
           style={[styles.addressBarTextInput, this.props.inputColor && {color: this.props.inputColor}]}
                             />
+        <View style={{marginLeft: 10, flexDirection: 'row', alignItems: 'flex-end'}}>
+          <Button
+            disabled={!this.state.backButtonEnabled}
+            onPress={this.state.onBack}>
+            <Image
+              style={this.buttonStyle()}
+              source={require('./assets/images/arrow-left.png')}
+                                />
+          </Button>
+
+          <Button
+            disabled={!this.state.forwardButtonEnabled}
+            onPress={this.state.onForward}>
+            <Image
+              style={this.buttonStyle()}
+              source={require('./assets/images/arrow-right.png')}
+                                />
+          </Button>
+        </View>
       </View>
     )
   }
